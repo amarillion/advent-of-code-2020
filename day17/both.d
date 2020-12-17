3891687662becba691f1d1384c72ba90e9f573c4
Martijn van Iersel <mvaniersel@gmail.com>
2020-12-17 14:07:45 +0100

    Even more generic

--- day17/both.d
+++ day17/both.d
@@ -3,26 +3,26 @@ import std.stdio;
 import std.string;
 import std.conv;
 
-struct vec(int N) {
-	int[N] val;
+struct vec(int N, V) {
+	V[N] val;
 	
-	@property int x() const { return val[0]; }
-	@property void x(int v) { val[0] = v; }
+	@property V x() const { return val[0]; }
+	@property void x(V v) { val[0] = v; }
 	
-	@property int y() const { return val[1]; }
-	@property void y(int v) { val[1] = v; }
+	@property V y() const { return val[1]; }
+	@property void y(V v) { val[1] = v; }
 
 	static if (N > 2) {
-		@property int z() const { return val[2]; }
-		@property void z(int v) { val[2] = v; }
+		@property V z() const { return val[2]; }
+		@property void z(V v) { val[2] = v; }
 	}
 
 	static if (N > 3) {
-		@property int w() const { return val[3]; }
-		@property void w(int v) { val[3] = v; }
+		@property V w() const { return val[3]; }
+		@property void w(V v) { val[3] = v; }
 	}
 
-	this(int x, int y, int z = 0, int w = 0) {
+	this(V x, V y, V z = 0, V w = 0) {
 		static if (N == 4) {
 			val = [x, y, z, w];
 		}
@@ -34,56 +34,63 @@ struct vec(int N) {
 		}
 	}
 
-	this(int init) {
+	this(V init) {
 		foreach (i; 0..N) {
 			val[i] = init;
 		}
 	}
 
-	void lowestCorner(vec!N p) {
+	void lowestCorner(U)(vec!(N, U) p) {
 		foreach (i; 0..N) {
 			if (p.val[i] < val[i]) { val[i] = p.val[i]; }
 		}
 	}
 
-	void highestCorner(vec!N p) {
+	void highestCorner(U)(vec!(N, U) p) {
 		foreach (i; 0..N) {
 			if (p.val[i] > val[i]) { val[i] = p.val[i]; }
 		}
 	}
 
-	vec!N opBinary(string op)(vec!N rhs) const if (op == "+") {
-		vec!N result;
+	/** addition */
+	vec!(N, V) opBinary(string op)(vec!(N, V) rhs) const if (op == "+") {
+		vec!(N, V) result;
 		foreach (i; 0..N) {
 			result.val[i] = val[i] + rhs.val[i];
 		}
 		return result;
 	}
 
-	vec!N opBinary(string op)(vec!N rhs) const if (op == "-") {
-		vec!N result;
+	/** substraction */
+	vec!(N, V) opBinary(string op)(vec!(N, V) rhs) const if (op == "-") {
+		vec!(N, V) result;
 		foreach (i; 0..N) {
 			result.val[i] = val[i] - rhs.val[i];
 		}
 		return result;
 	}
 
-	vec!N opBinary(string op)(int rhs) const if (op == "+") {
-		vec!N result;
+	/** add a scalar */
+	vec!(N, V) opBinary(string op)(V rhs) const if (op == "+") {
+		vec!(N, V) result;
 		foreach (i; 0..N) {
 			result.val[i] = val[i] + rhs;
 		}
 		return result;
 	}
 
-	vec!N opBinary(string op)(int rhs) const if (op == "-") {
-		vec!N result;
+	/** substract a scalar */
+	vec!(N, V) opBinary(string op)(V rhs) const if (op == "-") {
+		vec!(N, V) result;
 		foreach (i; 0..N) {
 			result.val[i] = val[i] - rhs;
 		}
 		return result;
 	}
 }
+
+alias vec3i = vec!(3, int);
+alias vec4i = vec!(4, int);
 
 struct CoordRange(T) {
 	
@@ -120,25 +127,28 @@ struct CoordRange(T) {
 
 }
 
-class SparseInfiniteGrid(T) {
+class SparseInfiniteGrid(T, U) {
 
-	bool[T] data;
+	U[T] data;
 	T min;
 	T max;
 
-	bool get(T p) {
+	U get(T p) {
 		if (p in data) {
 			return data[p];
 		}
 		else {
-			return false;
+			return U.init;
 		}
 	}
 
-	void set(T p, bool val) {
-		min.lowestCorner(p);
-		max.highestCorner(p);
-		data[p] = val;
+	void set(T p, U val) {
+		// we'll save a bit of space by not storing default values
+		if (val != U.init) {	
+			min.lowestCorner(p);
+			max.highestCorner(p);
+			data[p] = val;
+		}
 	}
 
 	void echo() {
@@ -160,66 +170,68 @@ class SparseInfiniteGrid(T) {
 		writeln();
 	}
 
-	void transform() {
-		auto newData = new SparseInfiniteGrid!T();
+	void transform(U delegate(T) transformCell) {
+		auto newData = new SparseInfiniteGrid!(T, U)();
 		foreach (p; CoordRange!T(min - 1, max + 1)) {
-			int count = 0;
-			foreach (d; CoordRange!T(T(-1), T(1))) {
-				if (d == T(0)) continue;
-				if (get(p + d)) {
-					count++;
-				}
-			}
-			const bool current = get(p);
-				
-			if ((current && count == 2) || count == 3) {
-				newData.set(p, true);				
-			}
+			newData.set(p, transformCell(p));
 		}
 		data = newData.data;
 		min = newData.min;
 		max = newData.max;		
 	}
+}
 
-	void readFromFile(string fname) {
-		File file = File(fname, "rt");
-
-		int y = 0;
-		while (!file.eof()) {
-			string line = chomp(file.readln()); 
-			if (line.length == 0) break;
-			int x = 0;
-			foreach (char ch; line) {
-				bool val = ch == '#';
-				T pos;
-				pos.x = x; pos.y = y;
-				set(pos, val);
-				x++;
-			}
-			y++;
+void readFromFile(T)(SparseInfiniteGrid!(T,bool) grid, string fname) {
+	File file = File(fname, "rt");
+	int y = 0;
+	while (!file.eof()) {
+		string line = chomp(file.readln()); 
+		if (line.length == 0) break;
+		int x = 0;
+		foreach (char ch; line) {
+			bool val = ch == '#';
+			T pos;
+			pos.x = x; pos.y = y;
+			grid.set(pos, val);
+			x++;
 		}
+		y++;
 	}
 }
 
 void main()
 {
+	bool transformCell(T, U)(U grid, T p) {
+		int count = 0;
+		foreach (d; CoordRange!T(T(-1), T(1))) {
+			if (d == T(0)) continue;
+			if (grid.get(p + d)) {
+				count++;
+			}
+		}
+		const bool current = grid.get(p);
+		return ((current && count == 2) || count == 3);
+	}
+
 	string fname = "input";
+	
 	// Part 1:
-	auto grid3 = new SparseInfiniteGrid!(vec!3)();
+	auto grid3 = new SparseInfiniteGrid!(vec3i, bool)();
 	grid3.readFromFile(fname);
 	
 	grid3.echo();
 	foreach (i; 0 .. 6) {
-		grid3.transform();
+		grid3.transform((p) => transformCell(grid3, p));
+		writeln(grid3.data.length);
 	}
 	grid3.echo();
 	
 	writeln("Part 1: ", grid3.data.length);
 
-	auto grid4 = new SparseInfiniteGrid!(vec!4)();
+	auto grid4 = new SparseInfiniteGrid!(vec4i, bool)();
 	grid4.readFromFile(fname);
 	foreach (i; 0 .. 6) {
-		grid4.transform();
+		grid4.transform((p) => transformCell(grid4, p));
 	}
 	// grid4.echo();
 	writeln("Part 2: ", grid4.data.length);
